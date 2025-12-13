753d85fc954778d6f9cb20703367fdb582595c81
salm1ina <nabilaakbars1@gmail.com>
2025-12-13 23:47:19 +0800

    ready for deployment

--- src/validators/category.validator.js
+++ src/validators/category.validator.js
@@ -8,7 +8,7 @@ export const updateCategorySchema = Joi.object({
   name: Joi.string().min(3).max(50).optional(),
 });
 
-// Middleware validator
+// middleware validator
 export function validateCategory(schema) {
   return (req, res, next) => {
     const { error } = schema.validate(req.body);
